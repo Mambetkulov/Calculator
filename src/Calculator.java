ecde543c4c17839f360ebca5504d89f2b170cb8d
Mambetkulov <muganm320@gmail.com>
2025-04-12 13:17:51 +0600

    text 3

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         System.out.println("text 1");
         System.out.println("text 2");
-        System.out.println("");
+        System.out.println("text 3");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
