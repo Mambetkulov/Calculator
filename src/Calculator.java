0236d56945639038e54481f1ff315a2fe43d7343
Mambetkulov <muganm320@gmail.com>
2025-04-14 11:54:41 +0600

    m2

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         System.out.println("sacred fire");
         System.out.println("s");
-        System.out.println("");
+        System.out.println("ss");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
