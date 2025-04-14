5384b226e75b279d580cd0129d6a2109cba926d9
Mambetkulov <muganm320@gmail.com>
2025-04-14 11:54:11 +0600

    m1

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("sacred fire");
-        System.out.println("");
+        System.out.println("s");
         System.out.println("");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
