62b6a974bcf43453ed7cde6badad06403e870d78
Mambetkulov <muganm320@gmail.com>
2025-04-23 12:37:10 +0600

    not2

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("");
-        System.out.println("h");
+        System.out.println("");
         System.out.println("r");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
