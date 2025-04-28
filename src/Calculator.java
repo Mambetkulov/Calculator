d4319fc1744e9f8a423ebe2b09afeccac29fb12c
Mambetkulov <muganm320@gmail.com>
2025-04-28 09:20:05 +0600

    nothing1

--- src/Calculator.java
+++ src/Calculator.java
@@ -100,7 +100,7 @@ public class Calculator implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("helo");
+        System.out.println("");
         System.out.println("halo");
         System.out.println("kon");
           for(int i = 0 ; i < 10 ; i++){
