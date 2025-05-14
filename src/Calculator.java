34f1ad349ad1f00c7bf81e607fe6146124ff9cbd
Mambetkulov <muganm320@gmail.com>
2025-05-14 14:39:22 +0600

    not

--- src/Calculator.java
+++ src/Calculator.java
@@ -100,7 +100,7 @@ public class Calculator implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("test1");
+        System.out.println("");
         System.out.println("test2");
         System.out.println("test");
           for(int i = 0 ; i < 10 ; i++){
