8feb91c7c7103c823c97f18b8d60b3d6625eb13c
Mambetkulov <muganm320@gmail.com>
2025-05-21 10:47:36 +0600

    undress

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         System.out.println("gd");
         System.out.println("nominate");
-        System.out.println("haha");
+        System.out.println("undress");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
