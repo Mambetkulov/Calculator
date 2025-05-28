66a330228819e516e7ae16ae750a71737c857365
Mambetkulov <muganm320@gmail.com>
2025-05-28 13:21:13 +0600

    banger

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("рщыд");
-        System.out.println("");
+        System.out.println("banger");
         System.out.println("tio");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
