c7c05b0d70c113e201aa92ee868006b357608a54
Mambetkulov <muganm320@gmail.com>
2025-05-28 13:21:38 +0600

    house

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("рщыд");
         System.out.println("banger");
-        System.out.println("tio");
+        System.out.println("house");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
