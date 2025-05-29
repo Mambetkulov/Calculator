0a4b462b7fc08fc58d9afe64860782b833223829
Mambetkulov <muganm320@gmail.com>
2025-05-29 18:56:47 +0600

    tyson

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("dos");
         System.out.println("bro");
-        System.out.println("house");
+        System.out.println("tysone");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
