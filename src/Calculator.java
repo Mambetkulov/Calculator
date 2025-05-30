3f49692f5cb8a43c41a6e06e9e1a6d0900176c5e
Mambetkulov <muganm320@gmail.com>
2025-05-30 09:37:31 +0600

    hitmen

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("chess");
         System.out.println("hover");
-        System.out.println("tysone");
+        System.out.println("hitmen");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
