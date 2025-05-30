53fbbbd4b70f348555182d8fae365b149d049f2e
Mambetkulov <muganm320@gmail.com>
2025-05-30 09:36:26 +0600

    chess

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("dos");
+        System.out.println("chess");
         System.out.println("bro");
         System.out.println("tysone");
           for(int i = 0 ; i < 10 ; i++){
