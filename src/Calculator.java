361b738f42acab793c43efa9198004dfc2479216
Mambetkulov <muganm320@gmail.com>
2025-06-01 21:06:31 +0600

    hisko

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("spider man");
         System.out.println("tor");
-        System.out.println("hesko");
+        System.out.println("hisko");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
