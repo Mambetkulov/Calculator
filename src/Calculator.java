04f0ad3cf9462298fdce25b8cc1eae48cb994908
Mambetkulov <muganm320@gmail.com>
2025-06-01 21:05:35 +0600

    tor

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("spider man");
-        System.out.println("chikago");
+        System.out.println("tor");
         System.out.println("hesko");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
