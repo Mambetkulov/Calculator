ba1e9c7b00a98a5ff56a5fbd9a39339fd7557879
Mambetkulov <muganm320@gmail.com>
2025-06-01 21:08:40 +0600

    bruh

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("spider man");
         System.out.println("tor");
-        System.out.println("");
+        System.out.println("bruh");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
