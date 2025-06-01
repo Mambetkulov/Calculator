019fcdd2e031a5f23b6746f495df45fad7f65160
Mambetkulov <muganm320@gmail.com>
2025-06-01 21:05:04 +0600

    spider man

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("broker");
+        System.out.println("spider man");
         System.out.println("chikago");
         System.out.println("hesko");
           for(int i = 0 ; i < 10 ; i++){
