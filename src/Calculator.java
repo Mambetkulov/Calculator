88200f4e62724ce085aef92c3867978f18cc37ea
Mambetkulov <muganm320@gmail.com>
2025-06-02 12:48:08 +0600

    brother

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("spider");
         System.out.println("hose");
-        System.out.println("bruh");
+        System.out.println("brother");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
