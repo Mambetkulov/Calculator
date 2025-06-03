46123f4e46c6451bb4a47cbce0baec43656dd84e
Mambetkulov <muganm320@gmail.com>
2025-06-03 11:01:07 +0600

    kity

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("walter");
         System.out.println("tyson");
-        System.out.println("brother");
+        System.out.println("kite");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
