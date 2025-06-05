29715ef6f436ce9e354bb764ad886349df65b0fc
Mambetkulov <muganm320@gmail.com>
2025-06-05 11:17:28 +0600

    fire

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("Kim bilet");
         System.out.println("nothing");
-        System.out.println("");
+        System.out.println("fire");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
