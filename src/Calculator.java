676537c50393471c9e1765ceff77bb1c3b787dea
Mambetkulov <muganm320@gmail.com>
2025-06-07 11:30:16 +0600

    what is it

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("");
+        System.out.println("what is it");
         System.out.println("");
         System.out.println("");
           for(int i = 0 ; i < 10 ; i++){
