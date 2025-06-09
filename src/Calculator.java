a71e72208000c6d431d4be13e0da7757dfadfb8b
Mambetkulov <muganm320@gmail.com>
2025-06-09 09:01:17 +0600

    notig

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("something");
+        System.out.println("");
         System.out.println("realy bother me");
         System.out.println("what is itgi");
           for(int i = 0 ; i < 10 ; i++){
