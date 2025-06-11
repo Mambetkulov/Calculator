a4953476458f13bb6ebd702a49408783a84986ae
Mambetkulov <muganm320@gmail.com>
2025-06-11 08:06:34 +0600

    no clue

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("gate");
         System.out.println("steins");
-        System.out.println("cruel");
+        System.out.println("no clue");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
