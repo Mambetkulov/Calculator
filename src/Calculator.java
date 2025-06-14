48bfe6e20e0ea31d8e66eb18842373f43f441928
Mambetkulov <muganm320@gmail.com>
2025-06-14 11:00:18 +0600

    hand

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("pizza");
-        System.out.println("here's my top 5 objects");
+        System.out.println("hand");
         System.out.println("brother");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
