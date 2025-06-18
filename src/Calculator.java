31a6502e8c31d39569a3e4752c0eb99951bad75c
Mambetkulov <muganm320@gmail.com>
2025-06-18 08:08:39 +0600

    got damit

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("what a hell");
-        System.out.println("what a fuck");
+        System.out.println("god damit");
         System.out.println("strike");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
