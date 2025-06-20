c1c10a983458926d6ea4457c6a18950f43affb59
Mambetkulov <muganm320@gmail.com>
2025-06-20 10:14:40 +0600

    true

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("human being in a mob");
+        System.out.println("true");
         System.out.println("rolling to the heaven");
         System.out.println("tr");
           for(int i = 0 ; i < 10 ; i++){
