dafe0f61653ef0f2b9aeb4c49c368209f3ff7ee4
Mambetkulov <muganm320@gmail.com>
2025-06-22 11:12:20 +0600

    tengen

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("what a hell");
-        System.out.println("beo");
+        System.out.println("tengen");
         System.out.println("wanka");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
