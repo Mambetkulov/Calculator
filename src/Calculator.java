8a9ddb06413c14512ede2239cc6674f0382472c8
Mambetkulov <muganm320@gmail.com>
2025-06-24 17:27:34 +0600

    java

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("python");
-        System.out.println("who knows");
+        System.out.println("java");
         System.out.println("h");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
