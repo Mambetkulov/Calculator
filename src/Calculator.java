14b88f063f4f248a935293932322ecde7fd49a1d
Mambetkulov <muganm320@gmail.com>
2025-06-24 17:27:06 +0600

    python

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("tyson");
+        System.out.println("python");
         System.out.println("who knows");
         System.out.println("h");
           for(int i = 0 ; i < 10 ; i++){
