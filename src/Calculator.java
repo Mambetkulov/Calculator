8e479831ee3e3007534155be1ac4f2dc8428d5d9
Mambetkulov <muganm320@gmail.com>
2025-06-27 11:10:11 +0600

    jujutsu kaisen

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("lock back");
-        System.out.println("naruto");
+        System.out.println("jujutsu kaisen");
         System.out.println("kakashi");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
