19068dfe37577e870c8acc2f30a48d8274bdb5fb
Mambetkulov <muganm320@gmail.com>
2025-06-29 14:50:52 +0600

    charlze

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("github");
+        System.out.println("charlze");
         System.out.println("you doesn't see me");
         System.out.println("poison");
           for(int i = 0 ; i < 10 ; i++){
