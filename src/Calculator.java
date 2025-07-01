3b74e035ac45d612df5de7be834d6f0099ce483d
Mambetkulov <muganm320@gmail.com>
2025-07-01 11:51:46 +0600

    video

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("music");
         System.out.println("image");
-        System.out.println("list");
+        System.out.println("video ");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
