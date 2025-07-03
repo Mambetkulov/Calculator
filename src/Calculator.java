1c354102e9e8b51939978002ed6783274247acc5
Mambetkulov <muganm320@gmail.com>
2025-07-03 14:17:12 +0600

    peddi

--- src/Calculator.java
+++ src/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         System.out.println("sui");
-        System.out.println("dog");
+        System.out.println("peddi");
         System.out.println("animal");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
