1d7308f8c4566288e9d3c4bf5bb5fcb4e7fad06d
Mambetkulov <muganm320@gmail.com>
2025-07-05 23:42:28 +0600

    cool

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("crud");
         System.out.println("dicipline");
-        System.out.println("earth");
+        System.out.println("cool");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
