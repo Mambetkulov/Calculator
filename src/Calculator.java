7085947272278d08898f9a7a9c3e29396ae67f67
Mambetkulov <muganm320@gmail.com>
2025-07-06 10:03:24 +0600

    when

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("crud");
+        System.out.println("when");
         System.out.println("dicipline");
         System.out.println("cool");
           for(int i = 0 ; i < 10 ; i++){
