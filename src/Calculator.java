bf52fc217a7d95442490cdce3243ad0308608a19
Mambetkulov <muganm320@gmail.com>
2025-07-10 15:59:46 +0600

    kubernate

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("docker");
         System.out.println("aws");
-        System.out.println("request");
+        System.out.println("kubernate");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
