71ca40192baf6df1662177223b534aa1818a1eb8
Mambetkulov <muganm320@gmail.com>
2025-07-11 15:02:04 +0600

    who knows

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,7 +101,7 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println("docker");
+        System.out.println("who knows");
         System.out.println("aws");
         System.out.println("kubernate");
           for(int i = 0 ; i < 10 ; i++){
