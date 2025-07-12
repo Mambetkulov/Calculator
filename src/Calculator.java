bc093d55e91cb2fcc476b756e528da44ca35da3f
Mambetkulov <muganm320@gmail.com>
2025-07-12 09:49:14 +0600

    new

--- src/Calculator.java
+++ src/Calculator.java
@@ -103,7 +103,7 @@ public class Calculator implements ActionListener {
         
         System.out.println("diary");
         System.out.println("captain");
-        System.out.println("old");
+        System.out.println("new");
           for(int i = 0 ; i < 10 ; i++){
               if(e.getSource()== numberButtons[i]){
                   textField.setText(textField.getText().concat(String.valueOf(i)));
